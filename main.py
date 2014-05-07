45f2c9e381dd1c32c46cd381fedce50db5a68971
Szymon Wanot <szwanot@gmail.com>
2014-05-07 14:54:42 +0200

    viev nearly complete, sound works wip

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.config import Config
 from kivy.animation import Animation
+from kivy.core.audio import SoundLoader, Sound
 import question
 
 class PlayWidget(Widget):
     current_question = NumericProperty(0)
     current_q_text = StringProperty('')
     current_a_list = ListProperty(['a','a','a','a'])
+    correct_sound = SoundLoader.load('sound/Fuzzy Beep-SoundBible.com-1580329899.wav')
+    wrong_sound = SoundLoader.load('sound/Computer Error-SoundBible.com-399240903.wav')
 
     def depackQuestionary(self, quiz_type):
         self.path = 'questionares/'+ quiz_type +'.json'
@@ -29,32 +32,48 @@ class PlayWidget(Widget):
         self.current_q_text = self.current_q.getText()
         self.current_answers = self.current_q.getAnswers()
         self.current_a_list = self.current_answers.keys()[:]
-        print 'questions: ', self.questionare.getQuestionsCount()
          
     def onClick(self, text):
-        self.parent.score.append(self.current_answers[text])
-        print self.current_answers[text] , self.current_question
+        value = self.current_answers[text]
+        self.parent.score.append(value)
+        if value == 'True':
+            self.correct_sound.play()
+        elif value == 'False':
+            self.wrong_sound.play()
         if self.current_question + 1 >= self.questionare.getQuestionsCount():
             self.current_question = 0
             self.parent.countScore()
-        
+            
+    def releaseSound(self):
+        if self.correct_sound.state == 'play':
+            self.correct_sound.stop()
+        if self.wrong_sound.state == 'play':
+            self.wrong_sound.stop()
+
 class StartScreen(Screen):
+    click_sound = SoundLoader.load('sound/Click2-Sebastian-759472264.mp3')
+    
     def on_pre_enter(self):
         self.bounce = Animation(size_hint= (0.35, 0.35))\
                 + Animation(size_hint= (0.3,0.3))
         self.bounce.repeat = True
         self.bounce.start(self.ids.owl)
+        self.bounce.start(self.ids.questions)
     def on_leave(self):
         self.bounce.cancel(self.ids.owl)
+        self.bounce.cancel(self.ids.questions)
+
+    def onClickSound(self):
+        self.click_sound.play()
 
 class ChooseQuiz(Screen):
+    click_sound = SoundLoader.load('sound/Click2-Sebastian-759472264.mp3')
+    
     def setOption(self, text):
         self.parent.option = text
-
-    def animateButtons(self, widget):
-        self.bounce = Animation(size = (0.9,0.9))\
-                        + Animation(size= (1.0,1.0))
-        self.bounce.start(widget)
+    
+    def onClickSound(self):
+        self.click_sound.play()
 
 class PlayScreen(Screen):
     quiz = ObjectProperty(None)
@@ -83,7 +102,8 @@ class PlayScreen(Screen):
 class HighScoreScreen(Screen):
     right_answers = NumericProperty(0)
     wrong_answers = NumericProperty(0)
-    
+    click_sound = SoundLoader.load('sound/Click2-Sebastian-759472264.mp3')
+
     def on_pre_enter(self):
         self.right_answers = self.parent.right_answers
         self.wrong_answers = self.parent.wrong_answers
@@ -93,6 +113,9 @@ class HighScoreScreen(Screen):
         self.wrong_answers = 0
         self.manager.right_answers = self.right_answers
         self.manager.wrong_answers = self.wrong_answers
+    
+    def onClickSound(self):
+        self.click_sound.play()
 
 class QuizApp(App):
     def build_config(self, config):
